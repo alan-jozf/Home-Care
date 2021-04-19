5ceff33c27dd7a09efbdc5ee58be388bc1ca6738
Alan Joseph <alanpezhumkattil@gmail.com>
2021-04-19 08:47:04 +0530

    password encryption

--- php/encryption/encryption.php
+++ php/encryption/encryption.php
@@ -36,3 +36,22 @@ cea50d07cf7d85354a579f13eb9e3281
 32: b6f1a6dc72a27526a692aff2e6031c24
 45: 000000000000000000000000000000000000000000000
 32: 63c5fcf83c2275fe64e880dd8dfc5cd6 -->
+
+
+<!-- decrption
+
+echo(chr(60))."<br>"; = <
+echo ord("<")."<br>"; = 60
+
+sprintf("%04d", 10); returns 0010
+$str = "00775505";
+$str = ltrim($str, "0"); 
+echo $str;
+775505
+
+
+
+
+
+
+ -->
